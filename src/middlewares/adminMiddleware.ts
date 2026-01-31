0afd89ab9acec8086e91f2e487481aecb19ca069
LuizMatt <luizmatheuslins5587@gmail.com>
2026-01-31 20:51:15 -0300

    refactor: deleting component

--- src/middlewares/adminMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Acesso negado. Apenas administradores.' });
-  }
-  
-  next();
-};
